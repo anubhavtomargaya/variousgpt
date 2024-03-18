ac70b06ef148e9f9e14e7b4efffd4f6f854dbb45
anubhav <imanubhav18@gmail.com>
2024-03-18 23:47:59 +0530

    cleanup

--- sbert/main.py
+++ sbert/main.py
@@ -1,7 +1,10 @@
+import gzip
+import json
+import os
 from sentence_transformers import SentenceTransformer,util, InputExample, losses
 from torch.utils.data import DataLoader
 
-
+import torch
 
 def embed():
     model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -93,5 +96,67 @@ def trainmodel():
     #Tune the model
     model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
 
+def sameplecosinsim():
+    embedder = SentenceTransformer('all-MiniLM-L6-v2')
 
-semanticsearch()
+    # Corpus with example sentences
+    corpus = ['A man is eating food.',
+            'A man is eating a piece of bread.',
+            'The girl is carrying a baby.',
+            'A man is riding a horse.',
+            'A woman is playing violin.',
+            'Two men pushed carts through the woods.',
+            'A man is riding a white horse on an enclosed ground.',
+            'A monkey is playing drums.',
+            'A cheetah is running behind its prey.'
+            ]
+    corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+
+    # Query sentences:
+    queries = ['A man is eating pasta.', 'Someone in a gorilla costume is playing a set of drums.', 'A cheetah chases prey on across a field.']
+
+
+    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    top_k = min(5, len(corpus))
+    for query in queries:
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
+
+        # We use cosine-similarity and torch.topk to find the highest 5 scores
+        cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
+        top_results = torch.topk(cos_scores, k=top_k)
+
+        print("\n\n======================\n\n")
+        print("Query:", query)
+        print("\nTop 5 most similar sentences in corpus:")
+
+        for score, idx in zip(top_results[0], top_results[1]):
+            print(corpus[idx], "(Score: {:.4f})".format(score))
+
+        """
+        # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
+        hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
+        hits = hits[0]      #Get the hits for the first query
+        for hit in hits:
+            print(corpus[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
+        """
+
+# semanticsearch()
+
+
+def importdataset():
+    wikipedia_filepath = './simplewiki-2020-11-01.jsonl.gz'
+
+    if not os.path.exists(wikipedia_filepath):
+        util.http_get('http://sbert.net/datasets/simplewiki-2020-11-01.jsonl.gz', wikipedia_filepath)
+
+    passages=[]
+    with gzip.open(wikipedia_filepath, 'rt', encoding='utf8') as fIn:
+        for line in fIn:
+            data = json.loads(line.strip())
+            for paragraph in data['paragraphs']:
+                # We encode the passages as [title, text]
+                passages.append([data['title'], paragraph])
+                
+    print("Passages:", len(passages))
+
+importdataset()
