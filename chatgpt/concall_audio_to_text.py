a5ae1d679969314465cf655be895bf745de3ad40
anubhav <imanubhav18@gmail.com>
2024-06-16 01:29:06 +0530

    audio to text working for audio mp3 file converted from video

--- chatgpt/concall_audio_to_text.py
+++ chatgpt/concall_audio_to_text.py
@@ -20,9 +20,10 @@ def create_text_from_audio(file_name,
 if __name__ == '__main__':
 
     def test_service_create_text_from_audio():
-        file = "Neuland_Laboratories_Ltd_Q4_FY2023-24_Earnings_Conference_Call.mp4"
-        base_prmpt_neu = "Neuland_Laboratories_Ltd_Q4_FY2023-24_Earnings_Conference_Call"
-
-        return create_text_from_audio(file_name=file,base_prompt=base_prmpt_neu,youtube=True)
+        # file = "Neuland_Laboratories_Ltd_Q4_FY2023-24_Earnings_Conference_Call.mp4"
+        # base_prmpt_neu = "Conference Call Insights Unlock the Future of Market Intelligence with AlphaStreet! Our revolutionary global ecosystem connects public companies, investors, analysts, and experts. At AlphaStreet, our mission is to empower our constituents to build robust connections and harness cutting-edge technology for insightful, data-driven decision making.\n\n\nDiscover the power of AI-driven AlphaStreet Intelligence, your gateway to services, research, and insights. Get real-time access to earnings and conference calls, interact with experts, engage with management/analysts/investor, all within one platform. Embrace innovation with our latest AI technology that allows for precision research from live and historical information and data. Zero in on insights from automatic topic categorization and on-demand summaries from a wealth of financial content. Additionally, experience generative AI for conversational interaction.\n\nAlphaStreet levels the playing field, giving you a strategic advantage in market intelligence. Join AlphaStreet, where we are uniting companies, analysts, investors, and experts to foster valuable interactions and shared insights."
+        file = "Juan_Camilo_Avendano_and_Ankit_Gupta.mp3"
+        base_prmpt = "INTERVIEW CALL BETWEEN TWO PEOPLE:" + file 
+        return create_text_from_audio(file_name=file,base_prompt=base_prmpt,youtube=False)
     
     print(test_service_create_text_from_audio())
