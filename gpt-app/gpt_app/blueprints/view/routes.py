2386b9953b7973265d9f15002ee1c78fa4b6acec
Anubha Tomar <imanubhav18@gmail.com>
2024-06-18 11:30:09 +0000

    template submit page added

--- gpt-app/gpt_app/blueprints/view/routes.py
+++ gpt-app/gpt_app/blueprints/view/routes.py
@@ -5,6 +5,10 @@ from . import view_app
 def index():
     return render_template('index.html')
 
+@view_app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
 @view_app.route('/<path:path>')
 def catch_all(path):
     return render_template('index.html')
