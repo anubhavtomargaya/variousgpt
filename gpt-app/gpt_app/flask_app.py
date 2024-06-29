0302561befa39e4080f6b876828f26d056db210a
anubhav <imanubhav18@gmail.com>
2024-06-30 02:30:50 +0530

    added error page and main page to the app

--- gpt-app/gpt_app/flask_app.py
+++ gpt-app/gpt_app/flask_app.py
@@ -5,9 +5,9 @@ function. They can be added as a blueprint but I am using these as tools while
 developement. Run this with wsgi.py" 
 """
 # from common import fh
-from flask import Flask, jsonify
+from flask import Flask, jsonify,redirect, render_template, url_for
 from flask_cors import CORS
-
+from gpt_app.common.session_manager import set_auth_state,set_auth_token, clear_auth_session, get_next_url,is_logged_in
 from gpt_app.blueprints.gptube import gpt_app
 from gpt_app.blueprints.google_auth import google_auth
 from gpt_app.blueprints.view import view_app
@@ -45,17 +45,33 @@ def create_app():
         # else:
             return jsonify(get_session_items())
         # return redirect(url_for('login'))  
-
-
+    google_auth_page = 'google_auth.login'
+    # print("url_for(google_auth_page)")
+    # print(url_for(google_auth_page))
+    default_home_page = 'view_app.index'
+    default_error_page = """<body >  
+                                <p>Oops... Login first</p>
+                                <br>
+                                <div style="text-align: center;"> 
+                                    <h1 style="color: #ffaaaa; font-size: 3em;">:(</h1>
+                                    <a href="{login}"> LOGIN </a>
+                                </div>
+                            </body>"""
+   
     
     ## added this route for startup help
     @app.route('/') 
     def index():
         sth = """ <p> - /google for google auth, fetching token for user & permissions etc. </br>
                     - /session route at __main__ for docs </br> 
-                    - 
+                    - /api/v1/gptube - app to serve the content with apis.
+                    - /view/ - app to mimic front end client
     """
-        return sth
+        if is_logged_in():
+            return redirect(url_for(default_home_page))
+        else:
+             return default_error_page.format(login=url_for(google_auth_page))
+
     return app
 
 app = create_app()
