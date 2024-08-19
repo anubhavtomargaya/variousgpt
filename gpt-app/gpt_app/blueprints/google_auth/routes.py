21703205474fb9b5691016f21034bd8149a22096
Anubha Tomar <imanubhav18@gmail.com>
2024-08-19 10:40:24 +0000

    https change

--- gpt-app/gpt_app/blueprints/google_auth/routes.py
+++ gpt-app/gpt_app/blueprints/google_auth/routes.py
@@ -31,10 +31,10 @@ def login():
     print("auth url")
     print(url_for('google_auth.google_auth_redirect',
                                             _external=True))
-    print('url base',AUTH_REDIRECT_URI)
+    print('url base',AUTH_REDIRECT_URI_HTTPS)
     session = OAuth2Session(CLIENT_ID, CLIENT_SECRET,
                             scope=AUTHORIZATION_SCOPE,
-                            redirect_uri=AUTH_REDIRECT_URI,
+                            redirect_uri=AUTH_REDIRECT_URI_HTTPS,
                             _external=True)
                             #   url_for('google_auth.google_auth_redirect',
                                                 #  _external=True)) #no need to use 
@@ -61,7 +61,7 @@ def google_auth_redirect():
                                 scope=AUTHORIZATION_SCOPE,
                                 state=req_state,
                                 redirect_uri=
-                                        AUTH_REDIRECT_URI)
+                                        AUTH_REDIRECT_URI_HTTPS)
     except Exception as e:
         return flask.jsonify(e)
     current_app.logger.debug(' session built')
