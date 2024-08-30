8fe3b7c11fcdd492922b5f0b9bb699ec2324c75b
Anubha Tomar <imanubhav18@gmail.com>
2024-08-30 15:58:18 +0000

    bing ka ek or domain

--- gpt-app/gpt_app/flask_app.py
+++ gpt-app/gpt_app/flask_app.py
@@ -89,6 +89,7 @@ def create_app():
             #  return default_error_page.format(login=url_for(google_auth_page))
 
     #sitemap
+    @app.route('/sitemap_index.xml')
     @app.route('/sitemap.xml.gz')
     @app.route('/sitemap.xml')
     def sitemap():
