b8593d06226542d761c2345e889bf1727347120a
Anubha Tomar <imanubhav18@gmail.com>
2024-08-30 13:17:01 +0000

    sitemap url for bing

--- gpt-app/gpt_app/flask_app.py
+++ gpt-app/gpt_app/flask_app.py
@@ -89,6 +89,7 @@ def create_app():
             #  return default_error_page.format(login=url_for(google_auth_page))
 
     #sitemap
+    @app.route('/sitemap.xml.gz')
     @app.route('/sitemap.xml')
     def sitemap():
         return send_from_directory('.', 'sitemap.xml')
